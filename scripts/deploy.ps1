e07b4485e75739949d32534bb73d4b4f8fe9d760
iceeburr <personal@iceeburr.ru>
2024-03-18 23:02:45 +0300

    autodeploy test 8

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,11 +1,10 @@
-$rbxcloudOutput = rbxcloud experience publish -f roblox-video-codec.rbxl -p ${env:PLACE_ID} -u ${env:GAME_ID} -t published -a 1020 2>&1
+$rbxcloudOutput = rbxcloud experience publish -f roblox-video-codec.rbxl -p ${env:PLACE_ID} -u ${env:GAME_ID} -t published -a ${env:DEPLOY_API_KEY} 2>&1
 
 if ($rbxcloudOutput -notlike "*published * with version number*") {
     Write-Host "::error::$rbxcloudOutput"
     exit 1
 }
 
-#${env:DEPLOY_API_KEY}
 #rbxcloud assets update --asset-id ${{ vars.ASSET_ID }} --filepath roblox-video-codec.rbxm --api-key ${{ secrets.DEPLOY_API_KEY }} --asset-type model-fbx
 
 $wallyOutput = wally login --token ${env:WALLY_TOKEN} 2>&1
