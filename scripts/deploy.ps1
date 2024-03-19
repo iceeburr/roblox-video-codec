e8344477cad2aaf07328ab4c2daa40108932e003
iceeburr <personal@iceeburr.ru>
2024-03-19 14:50:43 +0300

    autodeploy test 11

--- scripts/deploy.ps1
+++
@@ -1,8 +0,0 @@
-rbxcloud experience publish -f roblox-video-codec.rbxl -p ${env:PLACE_ID} -u ${env:GAME_ID} -t published -a 1010 #${env:DEPLOY_API_KEY}
-
-#rbxcloud assets update --asset-id ${{ vars.ASSET_ID }} --filepath roblox-video-codec.rbxm --api-key ${{ secrets.DEPLOY_API_KEY }} --asset-type model-fbx
-
-wally login --token ${env:WALLY_TOKEN}
-wally publish
-
-Write-Host "Deployed Successfully."
