edcfde53a278323617f339fb40e7cc294b56b838
iceeburr <personal@iceeburr.ru>
2024-04-01 00:02:21 +0300

    added the SOF0 marker

--- src/init.lua
+++ src/init.lua
@@ -52,34 +52,54 @@ function rbxvideo.new(videodata: buffer): videostream
 	local band = bit32.band
 	local bor = bit32.bor
 
-	local jpegData = { {} }
+	local jpegData: { any } = { {}, {}, {}, {} }
 
 	local function combineBytes(high: number, low: number): number
 		return bor(low, lshift(high, 8))
 	end
 
-	local function DQT(index: number)
+	local function DQT(index: number, length: number): ()
 		local quant: {} = table.create(64)
 		local header = readu8(videodata, index)
-		if rshift(header, 4) == 1 then
-			for j = 1, 64 do
-				quant[j] = combineBytes(readu8(videodata, index + j), readu8(videodata, index + j + 1))
-			end
-		else
+		if rshift(header, 4) == 0 then
 			for j = 1, 64 do
 				quant[j] = readu8(videodata, index + j)
+			end
+		else
+			warn("Warning - DQT has precision 1 (not tested)")
+			for j = 1, 128 do
+				quant[j] = combineBytes(readu8(videodata, index + j), readu8(videodata, index + j + 1))
 			end
 		end
 		jpegData[1][band(header, 0x0F) + 1] = quant
 	end
 
-	local function SOF0(index: number) end
+	local function SOF0(index: number, length: number): ()
+		local frameData: { any } = {
+			readu8(videodata, index),
+			combineBytes(readu8(videodata, index + 3), readu8(videodata, index + 4)), -- Width (xResolution)
+			combineBytes(readu8(videodata, index + 1), readu8(videodata, index + 2)), -- Heigth (yResolution)
+			{},
+		}
+		for i = 0, readu8(videodata, index + 5) - 1 do
+			local offset = index + (i * 3)
+			local sampleFactor = readu8(videodata, offset + 7)
+			frameData[4][i + 1] = {
+				bit32.band(bit32.rshift(sampleFactor, 4), 0x0F),
+				bit32.band(bit32.rshift(sampleFactor, 4), 0x0F),
+				readu8(videodata, offset + 8),
+			}
+		end
+		jpegData[2] = frameData
+	end
 
-	local function DHT(index: number) end
+	local function DHT(index: number, length: number) end
 
-	local function DRI(index: number) end
+	local function DRI(index: number, length: number): ()
+		jpegData[5] = combineBytes(readu8(videodata, index - 2), readu8(videodata, index - 1))
+	end
 
-	local function SOS(index: number) end
+	local function SOS(index: number, length: number) end
 
 	local procedures = {
 		[0xDB] = DQT,
@@ -99,23 +119,19 @@ function rbxvideo.new(videodata: buffer): videostream
 					local markerType = readu8(videodata, index + 1)
 					local procedure = procedures[markerType]
 					if procedure then
-						procedure(index + 4)
+						local length = combineBytes(readu8(videodata, index + 2), readu8(videodata, index + 3))
+						procedure(index + 4, length - 2)
 						if markerType == 0xDA then
 							break
 						elseif markerType == 0xDD then
 							index += 4
 							continue
 						end
-						index += 2 + combineBytes(readu8(videodata, index + 2), readu8(videodata, index + 3))
+						index += 2 + length
 					else
 						index += 2 + combineBytes(readu8(videodata, index + 2), readu8(videodata, index + 3))
 					end
 				else
-					warn("--// ERROR DEBUG INFORMATION \\--")
-					warn("Index:", index)
-					warn("Byte:", readu8(videodata, index))
-					warn("Previous byte:", readu8(videodata, index - 1))
-					warn("Next byte:", readu8(videodata, index + 1))
 					error("Internal error: lost buffer offset.")
 				end
 			end
--- tests/StarterCharacterScripts/Decode/init.client.lua
+++ tests/StarterCharacterScripts/Decode/init.client.lua
@@ -6,6 +6,14 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local base64 = require(ReplicatedStorage.Packages.base64)
 local rbxvideo = require(ReplicatedStorage.Packages["roblox-video-codec"])
 local videodata = base64.decode(buffer.fromstring(script:WaitForChild("video").Value))
+local test = require(ReplicatedStorage.Packages["roblox-video-codec"].test)
+
+local bytes = table.create(buffer.len(videodata))
+for i = 1, buffer.len(videodata) do
+	bytes[i] = buffer.readu8(videodata, i - 1)
+end
+
+local _jpegtest = test.CreateJPEGfromBytes(bytes, true, true, false)
 
 local editableImage = Instance.new("EditableImage")
 editableImage.Size = Vector2.new(1024, 576)
