dfdc2adfe70d2c513bcb055fac294a22d2080a01
iceeburr <personal@iceeburr.ru>
2024-04-01 00:01:23 +0300

    added some required modules i forgot to import

---
+++ src/test/BitStream.lua
@@ -0,0 +1,61 @@
+local module = {}
+
+function module.CreateBitStream(data)
+	local stream = {}
+
+	stream.data = data
+	stream.pos = 0
+
+	local nextByte = 0
+	local nextBit = 0
+
+	function stream.ReadBit()
+		if nextByte >= #stream.data then
+			return -1
+		end
+
+		local _index = bit32.rshift(stream.pos, 3) + 1
+
+		local bit = bit32.band(bit32.rshift(stream.data[nextByte + 1], (7 - nextBit)), 1)
+		nextBit = nextBit + 1
+
+		if nextBit == 8 then
+			nextBit = 0
+			nextByte = nextByte + 1
+		end
+
+		return bit
+	end
+
+	function stream.ReadBits(length)
+		local bits = 0
+
+		for i = 1, length do
+			local bit = stream.ReadBit()
+
+			if bit == -1 then
+				bits = -1
+				break
+			end
+
+			bits = bit32.bor(bit32.lshift(bits, 1), bit)
+		end
+
+		return bits
+	end
+
+	function stream.Align()
+		if nextByte >= #stream.data then
+			return
+		end
+
+		if nextBit ~= 0 then
+			nextBit = 0
+			nextByte = nextByte + 1
+		end
+	end
+
+	return stream
+end
+
+return module
---
+++ src/test/HuffmanTable.lua
@@ -0,0 +1,79 @@
+--!nocheck
+local module = {}
+
+local function BitsFromLength(root, element, pos)
+	if type(root) == "table" then
+		if pos == 0 then
+			if #root < 2 then
+				table.insert(root, element)
+				return true
+			end
+
+			return false
+		end
+
+		for i = 0, 1 do
+			if #root == i then
+				table.insert(root, {})
+			end
+
+			if BitsFromLength(root[i + 1], element, pos - 1) == true then
+				return true
+			end
+		end
+	end
+
+	return false
+end
+
+function module.CreateHuffmanTable()
+	local huff = {}
+
+	huff.root = {}
+	huff.symbols = {}
+
+	return huff
+end
+
+function module.ConstructTree(huffmanTable, lengths, elements)
+	huffmanTable.elements = elements
+	local ii = 0
+
+	for i = 1, #lengths do
+		for j = 1, lengths[i] do
+			BitsFromLength(huffmanTable.root, elements[ii + 1], i - 1)
+			ii = ii + 1
+		end
+	end
+end
+
+function module.GetNextSymbolFromHuffmanTable(huffmanTable, bitStream)
+	if huffmanTable == nil then
+		error("HUFFMAN TABLE IS NIL.")
+	end
+
+	local r = huffmanTable.root
+
+	local length = 0
+
+	while type(r) == "table" do
+		r = r[bitStream.ReadBit() + 1]
+		length = length + 1
+	end
+
+	return r, length
+end
+
+function module.GetCode(huffmanTable, bitStream)
+	while true do
+		local result = module.GetNextSymbolFromHuffmanTable(huffmanTable, bitStream)
+
+		if result == nil then
+			return -1
+		end
+
+		return result
+	end
+end
+
+return module
